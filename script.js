8df49cf3ebcaec6c6f25a11e93ee71d9f812aed3
Ali George <george7ali@gmail.com>
2023-07-25 00:28:17 +0100

    successfully linked new-book button and the new book form and added event listener

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(){
+
+}
+
+function addBookToLibrary(){
+    
+}
+let newBookBtn = document.querySelector('.newBook-btn');
+newBookBtn.addEventListener('click', function(){
+     let newBookForm = document.querySelector('.newBook-form')
+     newBookForm.style.display = 'block';
+})
