339f2af7f8ee356b2c59be07e54b7605063f7e47
Ali George <george7ali@gmail.com>
2023-07-25 03:43:59 +0100

    created a render function to enable the app display the items of my library on the html when adding book

--- script.js
+++ script.js
@@ -7,13 +7,35 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
+function render(){
+    let libraryEl = document.querySelector('#booksGrid');
+    libraryEl.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i];
+        let bookEl = document.createElement('div');
+        bookEl.innerHTML = `<p>${book.title}</p>`;
+        bookEl.innerHTML = `
+            <div class = 'card-header'>
+                <h3 class = 'title'>${book.title}</h3>
+                <h5 class = 'author'>by ${book.author}</h5>
+            </div>
+            <div class = 'card-body'>
+                <p>${book.pages} pages</p>
+                <p class = 'read-status'>${book.read ?'Read' : 'Not Yet'}</p>
+            </div>
+            `;
+        libraryEl.appendChild(bookEl)
+    }
+}
+
 function addBookToLibrary(){
    let title = document.querySelector('#title').value
    let author = document.querySelector('#author').value
    let pages = document.querySelector('#pages').value
    let reads = document.querySelector('#reads').value
    let newBook = new Book(title, author, pages, reads);
-   console.log(newBook); 
+   myLibrary.push(newBook);
+   render();
 }
 
 let newBookBtn = document.querySelector('.newBook-btn');
@@ -22,7 +44,7 @@ newBookBtn.addEventListener('click', function(){
      newBookForm.style.display = 'block';
 })
 
-document.querySelector('.newBook-form').addEventListener('submit', function(){
+document.querySelector('.newBook-form').addEventListener('submit', function(event){
     event.preventDefault()
     addBookToLibrary();
 })
