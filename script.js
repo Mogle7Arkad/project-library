8be35e56fec3a95e5a77ba5b404c62255c94d366
Ali George <george7ali@gmail.com>
2023-07-25 01:35:12 +0100

    successfullyinplemented the add book button to the addBookToLibrary and got a display result demo on the console

--- script.js
+++ script.js
@@ -8,10 +8,21 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(){
-    
+   let title = document.querySelector('#title').value
+   let author = document.querySelector('#author').value
+   let pages = document.querySelector('#pages').value
+   let reads = document.querySelector('#reads').value
+   let newBook = new Book(title, author, pages, reads);
+   console.log(newBook); 
 }
+
 let newBookBtn = document.querySelector('.newBook-btn');
 newBookBtn.addEventListener('click', function(){
      let newBookForm = document.querySelector('.newBook-form')
      newBookForm.style.display = 'block';
+})
+
+document.querySelector('.newBook-form').addEventListener('submit', function(){
+    event.preventDefault()
+    addBookToLibrary();
 })
